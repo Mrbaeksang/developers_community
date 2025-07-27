50bfa8eeeb3d3dfbca0d0edd6ed1271c5c2d09bd
Mrbaeksang <qortkdgus95@gmail.com>
2025-07-27 19:51:18 +0900

    fix: lint-staged Windows 설정을 JSON 파일로 변경

--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': (filenames) =>
-    `prettier --write ${filenames.map((file) => `"${file}"`).join(' ')}`,
-  '*.{json,css}': (filenames) =>
-    `prettier --write ${filenames.map((file) => `"${file}"`).join(' ')}`
-}
